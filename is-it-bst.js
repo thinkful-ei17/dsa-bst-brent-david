1d28dbbaded1128b5e2d3cca9008c526c65392dc
Brent Guistwite <brentguistwite@gmail.com>
2018-03-06 17:04:09 -0500

    work on is it bst

--- is-it-bst.js
+++ is-it-bst.js
@@ -1,4 +1,5 @@
 const bst = require('./bst.js');
+const util = require('util');
 // start at root
 // while (root.left !== null)
 // if root.left < root, go on to root.left
@@ -9,23 +10,43 @@ const bst = require('./bst.js');
 // if(root.left > root || root.right < root)
 // declare root value to compare to values on left and right
 
-const isItBst = tree => {
-  
+// ONLY SOMETING ON LEFT
+// ONLY SOMETHING ON RIGHT
+// LEFT < ROOT < RIGHT
+
+const isItBst = (tree, leftArr = [], rightArr=[]) => {
+  // NOTHING ON LEFT OR RIGHT OF ROOT
+  if (tree.left === null && tree.right === null) {
+    return true;
+  } else {     
+    if (tree.right || tree.left) {
+      console.log('only traverse right side ran');
+      if (isItBst(tree.left,[...leftArr, tree.key], rightArr) && isItBst(tree.right, leftArr, [...rightArr, tree.key])){
+        return true;
+      } else {
+        return false;
+      } 
+    } else if (tree.left) {
+      console.log('only traverse left');
+    } 
+  }
+  return true;
 };
 
 const newTree = new bst();
 
-newTree.insert(3, 3);
-newTree.insert(25, 25);
-newTree.insert(4, 4);
-newTree.insert(6, 6);
-newTree.insert(5, 5);
-newTree.insert(10, 10);
 newTree.insert(0, 0);
+newTree.insert(4, 4);
+newTree.insert(5, 5);
+newTree.insert(6, 6);
+// newTree.insert(5, 5);
+// newTree.insert(10, 10);
+// newTree.insert(0, 0);
 newTree.insert(7, 7);
-newTree.insert(2, 2);
-newTree.insert(27, 27);
-newTree.insert(26,26);
-newTree.insert(24, 24);
-
+// newTree.insert(2, 2);
+// newTree.insert(27, 27);
+// newTree.insert(26,26);
+// newTree.insert(24, 24);
 console.log(isItBst(newTree));
+console.log(util.inspect(newTree, { showHidden: true, depth: null }));
+
