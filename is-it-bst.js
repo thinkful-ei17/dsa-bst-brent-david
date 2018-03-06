bec2571ba5b9c48d14f1871d94a0c20b71e57dcf
David Park <dysp92@gmail.com>
2018-03-06 14:36:56 -0800

    still working on is-it-bst

--- is-it-bst.js
+++ is-it-bst.js
@@ -13,21 +13,50 @@ const util = require('util');
 // ONLY SOMETING ON LEFT
 // ONLY SOMETHING ON RIGHT
 // LEFT < ROOT < RIGHT
-
+// move the comparision to top 
+// instead of putting the array every recursive call, just input our newMax and newMin
 const isItBst = (tree, leftArr = [], rightArr=[]) => {
   // NOTHING ON LEFT OR RIGHT OF ROOT
   if (tree.left === null && tree.right === null) {
     return true;
   } else {     
-    if (tree.right || tree.left) {
-      console.log('only traverse right side ran');
-      if (isItBst(tree.left,[...leftArr, tree.key], rightArr) && isItBst(tree.right, leftArr, [...rightArr, tree.key])){
-        return true;
-      } else {
+    if (tree.right) {
+      console.log('traverse right');
+      //comparison stuff here
+      // if tree.key > Math.max(leftArr)
+      if (tree.key > Math.max(leftArr)) {
+        console.log('one');
         return false;
-      } 
-    } else if (tree.left) {
+      }
+      
+      if (tree.key < Math.min(rightArr)) {
+        console.log('two');
+        return false;
+      }
+      
+      console.log('three');
+      return isItBst(tree.right, leftArr, [...rightArr, tree.key]);
+      //eihter return false or pass through
+      // console.log('only traverse right side ran');
+      // if (isItBst(tree.left,[...leftArr, tree.key], rightArr) && isItBst(tree.right, leftArr, [...rightArr, tree.key])){
+      //   return true;
+      // } else {
+      //   return false;
+      // } 
+    } 
+    if (tree.left) {
       console.log('only traverse left');
+      for (let i=0; i<leftArr.length; i++) {
+        if (tree.key < leftArr[i]) {
+          return false;
+        }
+      }
+      for (let i=0; i<rightArr.length; i++) {
+        if (tree.key > rightArr[i]) {
+          return false;
+        }
+      }
+      return isItBst(tree.left, [...leftArr, tree.key], rightArr);
     } 
   }
   return true;
@@ -48,5 +77,5 @@ newTree.insert(7, 7);
 // newTree.insert(26,26);
 // newTree.insert(24, 24);
 console.log(isItBst(newTree));
-console.log(util.inspect(newTree, { showHidden: true, depth: null }));
+// console.log(util.inspect(newTree, { showHidden: true, depth: null }));
 
