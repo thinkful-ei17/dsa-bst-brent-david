2e123ee48b34f961264510cdc61451e254237d50
Brent Guistwite <brentguistwite@gmail.com>
2018-03-06 12:34:16 -0500

    add until inspect to see full object

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 const bst = require('./bst.js');
+const util = require('util');
+
 
 const newTree = new bst();
 
@@ -7,5 +9,11 @@ newTree.insert(1, 1);
 newTree.insert(4, 4);
 newTree.insert(6, 6);
 newTree.insert(5, 5);
+newTree.insert(10, 10);
+newTree.insert(0, 0);
+newTree.insert(7, 7);
+newTree.insert(2, 2);
 
-console.log(newTree);
+
+
+console.log(util.inspect(newTree, { showHidden: true, depth: null }));
