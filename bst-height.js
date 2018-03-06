57b68fee9effea22a1a08e257e8ef955c63baa03
Brent Guistwite <brentguistwite@gmail.com>
2018-03-06 12:47:44 -0500

    start algorithm for finding bst height

---
+++ bst-height.js
@@ -0,0 +1,14 @@
+const bst = require('./bst.js');
+const util = require('util');
+
+const findHeight = () => {
+
+};
+
+findHeight();
+
+
+// does root.left AND root.right exist?
+// if only right exists keep going to the right until null while keeping track of levels
+// ^ same for left side
+
--- test.js
+++ test.js
@@ -5,7 +5,7 @@ const util = require('util');
 const newTree = new bst();
 
 newTree.insert(3, 3);
-newTree.insert(1, 1);
+newTree.insert(25, 25);
 newTree.insert(4, 4);
 newTree.insert(6, 6);
 newTree.insert(5, 5);
@@ -13,6 +13,9 @@ newTree.insert(10, 10);
 newTree.insert(0, 0);
 newTree.insert(7, 7);
 newTree.insert(2, 2);
+newTree.insert(27, 27);
+newTree.insert(26,26);
+newTree.insert(24, 24);
 
 
 
