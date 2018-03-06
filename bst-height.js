e22f92049e4f2e6c3df26aa46c17237e34761f76
David Park <dysp92@gmail.com>
2018-03-06 11:50:27 -0800

    changed heights to 0 from null and added ternary

--- bst-height.js
+++ bst-height.js
@@ -9,38 +9,37 @@ newTree.insert(2, 2);
 newTree.insert(4, 4);
 newTree.insert(6, 6);
 newTree.insert(1, 1);
-// newTree.insert(5, 5);
-// newTree.insert(10, 10);
-// newTree.insert(0, 0);
-// newTree.insert(7, 7);
-// newTree.insert(2, 2);
-// newTree.insert(27, 27);
-// newTree.insert(26,26);
-// newTree.insert(24, 24);
-// newTree.insert(23, 23);
+newTree.insert(5, 5);
+newTree.insert(10, 10);
+newTree.insert(0, 0);
+newTree.insert(7, 7);
+newTree.insert(2, 2);
+newTree.insert(27, 27);
+newTree.insert(26,26);
+newTree.insert(24, 24);
+newTree.insert(23, 23);
 
 
 const findHeight = (tree) => {
   if (tree.right === null && tree.left === null) {
     return 1;
   }
-  let leftHeight = null;
-  let rightHeight = null;
+  let leftHeight = 0;
+  let rightHeight = 0;
   if (tree.left) {
     leftHeight = findHeight(tree.left);
   }
   if (tree.right) {
     rightHeight = findHeight(tree.right);
   }
-  if (leftHeight >= rightHeight) {
-    return leftHeight + 1;
-  } else {
-    return rightHeight +1;
-  }
-  // leftHeight > rightHeight ? leftHeight + 1: rightHeight + 1;
+  // if (leftHeight >= rightHeight) {
+  //   return leftHeight + 1;
+  // } else {
+  //   return rightHeight +1;
+  // }
+  return leftHeight >= rightHeight ? leftHeight + 1: rightHeight + 1;
 };
 
-console.log(newTree);
 console.log(findHeight(newTree));
 
 
